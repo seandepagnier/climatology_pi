755820b76cacb26bc801b6a379a1a44426c8cf68
Sean D'Epagnier <sean@depagnier.com>
2014-01-16 13:51:26 +0800

    author url

--- src/ClimatologyConfigDialog.cpp
+++ src/ClimatologyConfigDialog.cpp
@@ -387,7 +387,5 @@ void ClimatologyConfigDialog::OnEnabled( wxCommandEvent& event )
 
 void ClimatologyConfigDialog::OnAboutAuthor( wxCommandEvent& event )
 {
-/*
-      wxLaunchDefaultBrowser(_T("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=sean%40depagnier%2ecom&lc=US&item_name=climatology&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest"));
-*/
+    wxLaunchDefaultBrowser(_T(ABOUT_AUTHOR_URL));
 }
--- src/climatology_pi.h
+++ src/climatology_pi.h
@@ -40,6 +40,8 @@
 #define     MY_API_VERSION_MAJOR    1
 #define     MY_API_VERSION_MINOR    9
 
+#define ABOUT_AUTHOR_URL "http://seandepagnier.users.sourceforge.net"
+
 #include "../../../include/ocpn_plugin.h"
 
 #include "../../../include/wx/jsonreader.h"
