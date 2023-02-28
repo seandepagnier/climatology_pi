3a8509acf141661064aee9cc09d4c6f8e950f87f
Rick Gleason <fcgleason@tgp-architects.com>
2023-02-28 11:36:06 -0500

    add inc GL glx.h to climoverlayfactory

--- src/ClimatologyOverlayFactory.cpp
+++ src/ClimatologyOverlayFactory.cpp
@@ -33,21 +33,20 @@
 # include <OpenGL/gl3.h>
 #endif
 
-
 #ifdef __OCPN__ANDROID__
 #include <qopengl.h>
 #include "GL/gl_private.h"
 #endif
 
-
 #ifdef FLATPAK
 #include "GL/gl_private.h"
 #endif
 
-
 #ifdef USE_GLES2
 #include "GLES2/gl2.h"
 #endif
+
+#include <GL/glx.h>
 
 #include "climatology_pi.h"
 #include "gldefs.h"
